f7b07c5792f397a953acdb875a72a27e021b69f0
Daniel Fernando Rico <d.rico@mobiera.com>
2024-09-19 18:13:36 -0500

    feat: chat call offer implementation

--- src/utils/permissions.ts
+++ src/utils/permissions.ts
@@ -1,5 +1,6 @@
+import { t } from 'i18next'
 import { Alert, Linking } from 'react-native'
-import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions'
+import { check, request, PERMISSIONS, RESULTS, Permission } from 'react-native-permissions'
 
 import { logError } from './log'
 
@@ -8,32 +9,49 @@ import { IS_DEVICE_IOS } from '@2060/constants'
 const MICROPHONE_PERMISSION = IS_DEVICE_IOS ? PERMISSIONS.IOS.MICROPHONE : PERMISSIONS.ANDROID.RECORD_AUDIO
 const CAMERA_PERMISSION = IS_DEVICE_IOS ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA
 
-const checkMicrophonePermission = async () => {
+const permissionText: Partial<Record<Permission, string>> = {
+  [CAMERA_PERMISSION]: t('general.permissionNeededMessage', { permission: t('signUp.camera') }),
+  [MICROPHONE_PERMISSION]: t('general.permissionNeededMessage', { permission: t('general.microphone') }),
+}
+
+const checkPermission = async (permission: Permission) => {
   try {
-    const status = await check(MICROPHONE_PERMISSION)
+    const status = await check(permission)
     const isGranted = status === RESULTS.GRANTED
     return isGranted
   } catch (error) {
-    logError('Error getting microphone permission ', error)
+    logError(`Error getting ${permission} permission`, error)
     return false
   }
 }
 
-const askMicrophonePermission = async () => {
+const askPermission = async (permission: Permission) => {
   try {
-    const status = await request(MICROPHONE_PERMISSION)
+    const status = await request(permission)
     const isGranted = status === RESULTS.GRANTED
     if (status === RESULTS.BLOCKED) {
-      Alert.alert('permitelas', '', [
-        { text: 'Ir a ajuster', style: 'default', onPress: () => Linking.openSettings() },
-        { text: 'Cancelar', style: 'destructive' },
-      ])
+      Alert.alert(
+        IS_DEVICE_IOS ? permissionText[permission]! : '',
+        !IS_DEVICE_IOS ? permissionText[permission]! : '',
+        [
+          { text: t('general.cancel'), style: 'destructive' },
+          { text: t('general.settings'), style: 'default', onPress: () => Linking.openSettings() },
+        ],
+      )
     }
     return isGranted
   } catch (error) {
-    logError('Error asking microphone permission ', error)
+    logError(`Error asking ${permission} permission`, error)
     return false
   }
+}
+
+const checkMicrophonePermission = async () => {
+  return await checkPermission(MICROPHONE_PERMISSION)
+}
+
+const askMicrophonePermission = async () => {
+  return await askPermission(MICROPHONE_PERMISSION)
 }
 
 const handleMicrophonePermission = async () => {
@@ -45,40 +63,12 @@ const handleMicrophonePermission = async () => {
   }
 }
 
-const checkCameraPermission = async () => {
-  try {
-    const status = await check(CAMERA_PERMISSION)
-    const isGranted = status === RESULTS.GRANTED
-    return isGranted
-  } catch (error) {
-    logError('Error getting CAMERA permission ', error)
-    return false
-  }
-}
-
-const askCameraPermission = async () => {
-  try {
-    const status = await request(CAMERA_PERMISSION)
-    const isGranted = status === RESULTS.GRANTED
-    if (status === RESULTS.BLOCKED) {
-      Alert.alert('permitelas', '', [
-        { text: 'Ir a ajuster', style: 'default', onPress: () => Linking.openSettings() },
-        { text: 'Cancelar', style: 'destructive' },
-      ])
-    }
-    return isGranted
-  } catch (error) {
-    logError('Error asking CAMERA permission ', error)
-    return false
-  }
-}
-
 const handleCameraPermission = async () => {
-  const hasCameraPermission = await checkCameraPermission()
+  const hasCameraPermission = await checkPermission(CAMERA_PERMISSION)
   if (hasCameraPermission) {
     return hasCameraPermission
   } else {
-    return await askCameraPermission()
+    return await askPermission(CAMERA_PERMISSION)
   }
 }
 
