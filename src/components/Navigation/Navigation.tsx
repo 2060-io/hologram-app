c796b39b902515c9dad326e47ad1ef3308e8e6a7
Daniel Fernando Rico <d.rico@mobiera.com>
2024-11-21 11:46:47 -0500

    fix: create isScreenLockForcedDisabled to use in some use cases where the is no need to make screen lock

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -20,6 +20,7 @@ import { manageBackgroundChatEntryChanges } from '@2060/hooks/agent/chat'
 import { manageConnectionStateChangedEvent } from '@2060/hooks/agent/connections/manageConnectionStateChangedEvent'
 import { useConfig } from '@2060/hooks/providers/ConfigProvider'
 import { useLocalRealm } from '@2060/hooks/providers/RealmProvider'
+import { useScreenLock } from '@2060/hooks/providers/ScreenLockProvider'
 import {
   HomeMain,
   SignUpMain,
@@ -60,8 +61,8 @@ const Navigation = ({ isSignedUp, agent, theme }: NavigationProps) => {
   const { realm } = useLocalRealm()
   const styles = getStyles(theme)
   const globalStyles = getGlobalStyles(theme)
-  const { isDeveloperMode, isDoingNfcReaderSession } = useConfig()
-
+  const { isDeveloperMode } = useConfig()
+  const { isScreenLockForcedDisabled } = useScreenLock()
   const InitialComponent = isSignedUp ? HomeMain : isDeveloperMode ? SignUpMain : ProfileCreation
 
   useEffect(() => {
@@ -105,8 +106,8 @@ const Navigation = ({ isSignedUp, agent, theme }: NavigationProps) => {
       const { addConnectionChangeListener, removeConnectionChangeListener } =
         manageConnectionStateChangedEvent(agent)
       if (isForeground) log('App in foreground')
-      if (!isForeground && !isDoingNfcReaderSession) {
-        log('App in background and not reading nfc ... registering to events')
+      if (!isForeground && !isScreenLockForcedDisabled) {
+        log('App in background and not isScreenLockForcedDisabled ... registering to events')
         addChatEntryChangeListener()
         addConnectionChangeListener()
       }
@@ -117,7 +118,7 @@ const Navigation = ({ isSignedUp, agent, theme }: NavigationProps) => {
         removeConnectionChangeListener()
       }
     }
-  }, [agent, realm, isForeground, isDoingNfcReaderSession])
+  }, [agent, realm, isForeground, isScreenLockForcedDisabled])
 
   return (
     <NavigationContainer linking={deepLinking} theme={theme.isDarkMode ? DarkTheme : DefaultTheme}>
