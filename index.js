c51efdd87ebebbe25689e3b68a8463055888a039
Daniel Fernando Rico <d.rico@mobiera.com>
2024-12-09 12:35:08 -0500

    feat: add new option in dev mode to turn on or turn off push background notifications handler

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* eslint-disable prettier/prettier */
+/* eslint-disable import/newline-after-import */
 import messaging from '@react-native-firebase/messaging'
 import React from 'react'
 import { AppRegistry } from 'react-native'
@@ -10,15 +10,13 @@ import App from './src/App'
 import CustomToast from './src/components/CustomToast'
 import { backgroundPushNotificationHandler } from './src/services/backgroundPushNotificationHandler'
 import { getAreBackgroundNotificationsEnabled } from './src/utils/developer'
-
-(async function () {
+;(async function () {
   const persistedIsBackgroundNotificationsEnabled = await getAreBackgroundNotificationsEnabled()
-  if (persistedIsBackgroundNotificationsEnabled){
+  if (persistedIsBackgroundNotificationsEnabled) {
     // Register handler for FCM notifications when app is in quit state
     messaging().setBackgroundMessageHandler(backgroundPushNotificationHandler)
   }
 })()
-
 
 AppRegistry.registerComponent(appName, () => () => (
   <>
