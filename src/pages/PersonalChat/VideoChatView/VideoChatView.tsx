3ce2265923763452c581d90366707e657fed5480
Daniel Fernando Rico León <33880481+DanielFRico@users.noreply.github.com>
2025-05-07 16:08:48 -0500

    fix: remove "save" option to messages type VoiceNote (#215)

--- src/pages/PersonalChat/VideoChatView/VideoChatView.tsx
+++ src/pages/PersonalChat/VideoChatView/VideoChatView.tsx
@@ -49,7 +49,7 @@ const VideoChatView = memo((props: MediaProps) => {
     mediaUploadState === MediaUploadState.ErrorCreating ||
     mediaUploadState === MediaUploadState.ErrorUploading
 
-  const videoFileUri = getLocalFileUri(localFilePath)
+  const videoFileUri = localFilePath ? getLocalFileUri(localFilePath) : undefined
   const localPreviewSource = localPreviewFilePath
     ? {
         uri: getLocalFileUri(localPreviewFilePath),
