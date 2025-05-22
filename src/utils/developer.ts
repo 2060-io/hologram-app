5bfa2929860ef713aed3f14dae0a6d9448acb58a
Ariel Gentile <gentilester@gmail.com>
2025-05-22 17:40:54 -0300

    fix: remove staging backend
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- src/utils/developer.ts
+++ src/utils/developer.ts
@@ -32,27 +32,23 @@ export const devEnvPlaceholder: DevEnvsObject = {
 export const allDevEnvs: DevEnv[] = [
   {
     key: 'CLOUD_AGENT_PUBLIC_DID',
-    values: ['did:web:ca.dev.2060.io', 'did:web:ca.st.2060.io', 'did:web:ca.2060.io'],
+    values: ['did:web:ca.dev.2060.io', 'did:web:ca.2060.io'],
   },
   {
     key: 'DATA_STORE_URL',
-    values: ['https://ds.dev.2060.io', 'https://ds.st.2060.io', 'https://ds.2060.io'],
+    values: ['https://ds.dev.2060.io', 'https://ds.2060.io'],
   },
   {
     key: 'TRUSTED_SERVICE_RESOLVER_BASE_URL',
-    values: ['https://tsr.dev.2060.io', 'https://tsr.st.2060.io', 'https://tsr.2060.io'],
+    values: ['https://tsr.dev.2060.io', 'https://tsr.2060.io'],
   },
   {
     key: 'WEBRTC_SERVER_BASE_URL',
-    values: ['https://webrtc.dev.2060.io', 'https://webrtc.st.2060.io', 'https://webrtc.2060.io'],
+    values: ['https://webrtc.dev.2060.io', 'https://webrtc.2060.io'],
   },
   {
     key: 'INDY_VDR_PROXY_BASE_URL',
-    values: [
-      'https://indyvdrproxy.ca.dev.2060.io',
-      'https://indyvdrproxy.ca.st.2060.io',
-      'https://indyvdrproxy.ca.2060.io',
-    ],
+    values: ['https://indyvdrproxy.ca.dev.2060.io', 'https://indyvdrproxy.ca.2060.io'],
   },
 ]
 
