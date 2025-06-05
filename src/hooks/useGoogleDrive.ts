6ce3993e11c130bfcffb9c950693badbcd4171bf
Daniel Fernando Rico <d.rico@mobiera.com>
2025-06-05 17:19:53 -0500

    fix: add console log output while downloading backup and decrease progress interval to 5 seconds

--- src/hooks/useGoogleDrive.ts
+++ src/hooks/useGoogleDrive.ts
@@ -199,13 +199,14 @@ export const useGoogleDrive = () => {
       try {
         const { promise } = downloadFile({
           fromUrl: backupHandler?.backup?.downloadUrl ?? '',
-          progressInterval: 10000,
+          progressInterval: 5000,
           headers: { Authorization: `Bearer ${googleDriveConnection?.accessToken}` },
           toFile: BACKUP_ZIP_FILE_PATH,
           progress: res => {
             const progress = Number(((res.bytesWritten / res.contentLength) * 100).toFixed())
             const progressLessOne = progress ? progress - 1 : progress
             setRestoreProgress(prev => ({ ...prev, progress: progressLessOne }))
+            log(`Downloading backup progress: ${progress}%`)
           },
         })
         await promise
