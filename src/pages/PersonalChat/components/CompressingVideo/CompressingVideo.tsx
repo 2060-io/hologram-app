d430b31bff7e64ffcc3033dc4d13aafdc10f5a32
Daniel Fernando Rico <d.rico@mobiera.com>
2025-06-13 17:01:47 -0500

    fix: add Progress indicator while compressing video

--- src/pages/PersonalChat/components/CompressingVideo/CompressingVideo.tsx
+++ src/pages/PersonalChat/components/CompressingVideo/CompressingVideo.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { ActivityIndicator, StyleSheet, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
-import { Text } from '@2060/components/common'
+import { Progress, Text } from '@2060/components/common'
 import { useTheme } from '@2060/hooks/providers/ThemeProvider'
 import { AppTheme } from '@2060/styles'
 
@@ -12,11 +12,11 @@ const CompressingVideo = ({ progress }: { progress: number }) => {
   const styles = getStyles(theme)
   return (
     <View style={styles.container}>
-      <ActivityIndicator size="large" color={theme.colors.green} />
       <Text
         typography="EuclidCircularA-Regular"
         style={styles.text}
       >{`${t('personalChat.processingVideo')} ${progress}%`}</Text>
+      <Progress progress={progress} progressColor={theme.colors.green} />
     </View>
   )
 }
@@ -28,10 +28,12 @@ const getStyles = (theme: AppTheme) =>
       backgroundColor: 'rgba(0,0,0,0.53)',
       justifyContent: 'center',
       alignItems: 'center',
+      paddingHorizontal: 12,
     },
     text: {
       color: theme.colors.white,
       fontSize: theme.fontSize.md,
+      marginBottom: 6,
     },
   })
 
