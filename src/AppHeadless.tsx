750cde2aaf5c6eb2600b5983bc4374b782b6ca06
Daniel Fernando Rico <d.rico@mobiera.com>
2025-06-26 14:45:28 -0500

    fix: add documentation link about Headless

--- src/AppHeadless.tsx
+++ src/AppHeadless.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import App from './App'
 import CustomToast from './components/CustomToast'
 
+// See documentation about Headless: https://rnfirebase.io/messaging/usage#background-application-state
 const AppHeadless = ({ isHeadless }: { isHeadless: boolean }) => {
   if (isHeadless) return null
   return (
