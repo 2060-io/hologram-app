917184dcb3a26be8db6cc25f53f7e4464f63e4b1
Daniel Fernando Rico <d.rico@mobiera.com>
2025-07-07 17:57:35 -0500

    fix: improve useWallet life cycle

--- src/components/Navigation/NavigationContainer.tsx
+++ src/components/Navigation/NavigationContainer.tsx
@@ -9,7 +9,6 @@ import { Loader } from '@2060/components/common'
 import { useWallet } from '@2060/hooks'
 import { useNavigation, useMobileAgent } from '@2060/hooks/agent'
 import { useTheme } from '@2060/hooks/providers/ThemeProvider'
-import { KeyChainService, retrieveEncryptedKey } from '@2060/services/keys'
 import { language } from '@2060/utils/language'
 
 const NavigationContainer = (Navigation: ElementType) => {
@@ -20,18 +19,14 @@ const NavigationContainer = (Navigation: ElementType) => {
     const { getLocalAuth } = useNavigation()
     const isAuthenticated = getLocalAuth()
     const { isSignedUp, agent } = useMobileAgent()
-    const { openingWallet, openWallet } = useWallet()
+    const { openingWallet, openWallet } = useWallet(true)
 
     useEffect(() => {
       i18n.changeLanguage(language)
     }, [])
 
     useEffect(() => {
-      const checkIfCanOpenWallet = async () => {
-        const walletKey = await retrieveEncryptedKey(KeyChainService.AfjWallet)
-        if (walletKey) openWallet()
-      }
-      if (isAuthenticated) checkIfCanOpenWallet()
+      if (isAuthenticated) openWallet()
     }, [isAuthenticated])
 
     return (
--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -3,21 +3,28 @@ import { useCallback, useState } from 'react'
 import { useMobileAgent } from './agent'
 import { useLocalRealm } from './providers/RealmProvider'
 
-import { log } from '@2060/utils'
+import { KeyChainService, retrieveEncryptedKey } from '@2060/services/keys'
+import { logWarn } from '@2060/utils'
 
-export const useWallet = () => {
-  const [openingWallet, setOpeningWallet] = useState(false)
+export const useWallet = (isOpeningWalletDefaultValue = false) => {
+  const [openingWallet, setOpeningWallet] = useState(isOpeningWalletDefaultValue)
   const { agent, openAndInitMobileAgent } = useMobileAgent()
   const { openRealm } = useLocalRealm()
+
+  const checkIfCanOpenWallet = async () => {
+    const walletKey = await retrieveEncryptedKey(KeyChainService.AfjWallet)
+    return walletKey
+  }
 
   const openWallet = useCallback(async () => {
     if (!agent) return
     try {
-      setOpeningWallet(true)
+      const canOpenWallet = await checkIfCanOpenWallet()
+      if (!canOpenWallet) throw new Error('No wallet key stored, so wallet can not be opened')
       await openAndInitMobileAgent()
       await openRealm()
     } catch (error) {
-      log(`wallet opening error: ${error}`)
+      logWarn(`wallet opening error: ${error}`)
     } finally {
       setOpeningWallet(false)
     }
