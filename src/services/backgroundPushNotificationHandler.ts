2c4fa8c099133a01772582ee6362299a56fba961
Daniel Fernando Rico <d.rico@mobiera.com>
2025-07-11 15:43:17 -0500

    fix: set correct assertion to do not process PN is app has no allowed them

--- src/services/backgroundPushNotificationHandler.ts
+++ src/services/backgroundPushNotificationHandler.ts
@@ -12,7 +12,7 @@ import { arePushNotificationsAllowed, deleteRemoteNotifications } from '@2060/ut
 
 const makeRequestToLocalServer = (payload: Record<string, string>) => {
   if (__DEV__) {
-    fetch('http://192.168.1.8:3000/api/echo', {
+    fetch('http://192.168.1.9:3000/api/echo', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(payload),
@@ -24,7 +24,7 @@ let isProcessingBackgroundNotification = false
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function backgroundPushNotificationHandler(remoteMessage: FirebaseMessagingTypes.RemoteMessage) {
-  if (await arePushNotificationsAllowed()) return
+  if (!(await arePushNotificationsAllowed())) return
   deleteRemoteNotifications()
   if (isProcessingBackgroundNotification) {
     makeRequestToLocalServer({ data: 'BACKGROUND PUSH NOTIFICATIONS HANDLER is executing at the moment!!' })
