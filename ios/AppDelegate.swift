b5bce2b6cf97d2c04dee6097da24143726d50a7e
Daniel Fernando Rico <d.rico@mobiera.com>
2025-08-13 19:00:31 -0500

    chore: bump to react-native 0.79.5

---
+++ ios/AppDelegate.swift
@@ -0,0 +1,81 @@
+import UIKit
+import FirebaseCore
+import React
+import React_RCTAppDelegate
+import ReactAppDependencyProvider
+import Firebase
+import Expo
+
+@main
+class AppDelegate: ExpoAppDelegate {
+  var window: UIWindow?
+ 
+  var reactNativeDelegate: ReactNativeDelegate?
+  var reactNativeFactory: RCTReactNativeFactory?
+ 
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    RNFBAppCheckModule.sharedInstance()
+    FirebaseApp.configure()
+    let delegate = ReactNativeDelegate()
+    let factory = ExpoReactNativeFactory(delegate: delegate)
+    delegate.dependencyProvider = RCTAppDependencyProvider()
+ 
+    reactNativeDelegate = delegate
+    reactNativeFactory = factory
+    bindReactNativeFactory(factory)
+    window = UIWindow(frame: UIScreen.main.bounds)
+ 
+    factory.startReactNative(
+      withModuleName: "hologram",
+      in: window,
+      initialProperties: RNFBMessagingModule.addCustomProps(
+        toUserProps: nil,
+        withLaunchOptions: launchOptions
+      ),
+      launchOptions: launchOptions
+    )
+ 
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  // Linking API
+  public override func application(
+    _ app: UIApplication,
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    let urlString = url.absoluteString
+    if urlString.hasPrefix("ShareMenuModule") {
+      super.application(app, open: url, options: options) || ShareMenuManager.application(app, open: url, options: options)
+    }
+    return super.application(app, open: url, options: options) || RCTLinkingManager.application(app, open: url, options: options)
+  }
+
+  // Universal Links
+  public override func application(
+    _ application: UIApplication,
+    continue userActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+  ) -> Bool {
+    let result = RCTLinkingManager.application(application, continue: userActivity, restorationHandler: restorationHandler)
+    return super.application(application, continue: userActivity, restorationHandler: restorationHandler) || result
+  }
+  
+}
+ 
+class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    // needed to return the correct URL for expo-dev-client.
+    bridge.bundleURL ?? bundleURL()
+  }
+ 
+  override func bundleURL() -> URL? {
+#if DEBUG
+    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
+}
--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,7 @@ const { getDefaultConfig } = require('expo/metro-config')
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = {
   transformer: {
