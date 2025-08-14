1a2e2f3b50ef87c99a649cd3aa905d29c9f80a47
Daniel Fernando Rico <d.rico@mobiera.com>
2025-08-14 17:48:09 -0500

    fix: add UI improvements mainly for bottom content in android where  content was so sticky to the end of screen

--- src/pages/Invitation/ConnectionInvitation.tsx
+++ src/pages/Invitation/ConnectionInvitation.tsx
@@ -2,7 +2,7 @@ import { StackActions } from '@react-navigation/native'
 import { StackScreenProps } from '@react-navigation/stack'
 import React, { useLayoutEffect, useState, useRef, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { TouchableOpacity, View, ScrollView } from 'react-native'
+import { TouchableOpacity, View, ScrollView, SafeAreaView } from 'react-native'
 
 import AlreadyConnected from './AlreadyConnected'
 import PublicService from './PublicService'
@@ -140,64 +140,66 @@ const ConnectionInvitation: React.FC<Props> = ({ navigation, route }: Props) => 
   useLayoutEffect(handleChangeHeaderOptions, [canConnect, theme.colors])
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <ModalLoading visible={isAcceptingInvitation} />
-      <View style={styles.root}>
-        {isAlreadyConnected && (
-          <AlreadyConnected
-            navigation={navigation}
-            connectionId={existingConnectionId}
-            includeDefaultActions={true}
-          />
-        )}
-        {invitationType === 'public' ? (
-          <PublicService
-            did={invitationDid}
-            initialServiceInfo={serviceInfo.current}
-            setAgeRestricted={setAgeRestricted}
-            userName={userProfileData?.displayName}
-          />
-        ) : (
-          <View>
-            <View style={styles.card}>
-              <Avatar uri={invitation?.imageUrl} label={invitation?.label} size="25%" withBorder={true} />
-              <Text typography="EuclidCircularA-Medium" style={styles.invitationLabel}>
-                {invitation?.label}
-              </Text>
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <ModalLoading visible={isAcceptingInvitation} />
+        <View style={styles.subContainer}>
+          {isAlreadyConnected && (
+            <AlreadyConnected
+              navigation={navigation}
+              connectionId={existingConnectionId}
+              includeDefaultActions={true}
+            />
+          )}
+          {invitationType === 'public' ? (
+            <PublicService
+              did={invitationDid}
+              initialServiceInfo={serviceInfo.current}
+              setAgeRestricted={setAgeRestricted}
+              userName={userProfileData?.displayName}
+            />
+          ) : (
+            <View>
+              <View style={styles.card}>
+                <Avatar uri={invitation?.imageUrl} label={invitation?.label} size="25%" withBorder={true} />
+                <Text typography="EuclidCircularA-Medium" style={styles.invitationLabel}>
+                  {invitation?.label}
+                </Text>
+                {invitationType === 'peer' && (
+                  <Text style={styles.content}>
+                    {t('invitation.peerInvitationDescription', { label: invitation?.label })}
+                  </Text>
+                )}
+                {invitationType === 'subInvitation' && (
+                  <Text typography="EuclidCircularA-Regular" style={styles.content}>
+                    {t('invitation.subConnectionInvitationDescription')}{' '}
+                    <Text typography="EuclidCircularA-Bold" style={styles.fontFamilyBold}>
+                      {`${invitation?.label} `}{' '}
+                    </Text>
+                    {t('invitation.subConnectionInvitationDescriptionAs')}{' '}
+                    <Text typography="EuclidCircularA-Bold" style={styles.fontFamilyBold}>
+                      {parentConnectionName}
+                    </Text>
+                  </Text>
+                )}
+              </View>
               {invitationType === 'peer' && (
-                <Text style={styles.content}>
-                  {t('invitation.peerInvitationDescription', { label: invitation?.label })}
-                </Text>
-              )}
-              {invitationType === 'subInvitation' && (
-                <Text typography="EuclidCircularA-Regular" style={styles.content}>
-                  {t('invitation.subConnectionInvitationDescription')}{' '}
-                  <Text typography="EuclidCircularA-Bold" style={styles.fontFamilyBold}>
-                    {`${invitation?.label} `}{' '}
+                <View style={styles.card}>
+                  <Text typography="EuclidCircularA-Regular" style={styles.enabledChannelsText}>
+                    {`${invitation?.label} ${t('invitation.enabledCommunicationChannelsDescription')}`}
                   </Text>
-                  {t('invitation.subConnectionInvitationDescriptionAs')}{' '}
-                  <Text typography="EuclidCircularA-Bold" style={styles.fontFamilyBold}>
-                    {parentConnectionName}
-                  </Text>
-                </Text>
+                  <View style={styles.separator} />
+                  <CommunicationChannels
+                    channels={communicationChannels}
+                    setChannels={setCommunicationChannels}
+                  />
+                </View>
               )}
             </View>
-            {invitationType === 'peer' && (
-              <View style={styles.card}>
-                <Text typography="EuclidCircularA-Regular" style={styles.enabledChannelsText}>
-                  {`${invitation?.label} ${t('invitation.enabledCommunicationChannelsDescription')}`}
-                </Text>
-                <View style={styles.separator} />
-                <CommunicationChannels
-                  channels={communicationChannels}
-                  setChannels={setCommunicationChannels}
-                />
-              </View>
-            )}
-          </View>
-        )}
-      </View>
-    </ScrollView>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
