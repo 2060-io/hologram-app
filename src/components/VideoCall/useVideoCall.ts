0ed3d0fca7b59360970687d22641d7b840d16ee3
Daniel Fernando Rico León <33880481+DanielFRico@users.noreply.github.com>
2025-09-22 10:17:33 -0500

    fix: update calloffer to fisnish when services finishes call

--- src/components/VideoCall/useVideoCall.ts
+++ src/components/VideoCall/useVideoCall.ts
@@ -6,12 +6,13 @@ import {
 } from 'expo-screen-orientation'
 import { Device, types } from 'mediasoup-client'
 import { WebSocketTransport, Peer } from 'protoo-client'
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import InCallManager from 'react-native-incall-manager'
 import { MediaStream, mediaDevices, registerGlobals } from 'react-native-webrtc'
 
 import { AgentActionType, useMobileAgent } from '@2060/hooks/agent'
+import { findAllDidcommThreadId, updateChatEntryMetadata } from '@2060/hooks/agent/chat/services'
 import { useAgentActionQueue } from '@2060/hooks/agent/useAgentActionQueue'
 import { useConfig } from '@2060/hooks/providers/ConfigProvider'
 import { useLocalRealm } from '@2060/hooks/providers/RealmProvider'
@@ -21,6 +22,7 @@ import {
   useVideoCallContext,
   isIncomingCallInfo,
 } from '@2060/hooks/providers/useVideoCallContext'
+import { CallOfferMetadata, CallOfferState, ChatEntryType } from '@2060/model'
 import { log, logError } from '@2060/utils'
 import { getAppCheckHeaders } from '@2060/utils/firebaseUtils'
 
@@ -133,6 +135,18 @@ export const useVideoCall = () => {
   const newRemotePeerLastConnection = useRef<Date>(undefined)
   const { devEnvs } = useConfig()
 
+  const updateChatEntryCallOfferMetadata = useCallback(() => {
+    if (!realm || !didcommThreadId) return
+    const [chatEntry] = findAllDidcommThreadId(realm, didcommThreadId, ChatEntryType.CallOffer)
+    if (chatEntry) {
+      const newMetadata = {
+        ...chatEntry.metadata,
+        state: CallOfferState.FINISHED,
+      } as CallOfferMetadata
+      updateChatEntryMetadata(realm, chatEntry.id, newMetadata)
+    }
+  }, [realm, didcommThreadId])
+
   useEffect(() => {
     const initialize = async () => {
       if (!agent || !didcommConnection || !didcommCallType) return
@@ -210,6 +224,7 @@ export const useVideoCall = () => {
             case 'peerLeft': {
               log('other peer left call', notification.data)
               finishCall()
+              updateChatEntryCallOfferMetadata()
               break
             }
             case 'producerScore': {
