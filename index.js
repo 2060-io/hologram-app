779781c920e113aca09a904ee3881ac53fea5a4a
Daniel Fernando Rico <d.rico@mobiera.com>
2025-10-01 11:05:48 -0500

    fix: add maximumFileSize to 10MB and remove files name generation logic to avoid unexpected behaviors in file logic generation

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { name as appName } from './app.json'
 import AppHeadless from './src/AppHeadless'
 import { backgroundPushNotificationHandler } from './src/services/backgroundPushNotificationHandler'
 
+const TEN_MB = 1024 * 1024 * 10
 import { LOGS_DIRECTORY, log, logError } from '@2060/utils/log'
 // Register handler for FCM notifications when app is in quit state
 const messaging = getMessaging()
@@ -16,6 +17,7 @@ AppRegistry.registerComponent(appName, () => AppHeadless)
 FileLogger.configure({
   logsDirectory: LOGS_DIRECTORY,
   logPrefix: 'hologram',
+  maximumFileSize: TEN_MB,
 })
   .then(() => log('react-native-file-logger setup!'))
   .catch(error => logError(`An error has occurred configuring react-native-file-logger: ${error}`))
