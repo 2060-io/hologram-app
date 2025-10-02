00bd81953e18970b10546b4cef03440ac8d42b52
Daniel Fernando Rico <d.rico@mobiera.com>
2025-10-01 19:56:00 -0500

    fix: display console.error and console.warn even in production mode to track them in log files if error or warns are executed

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -11,16 +11,12 @@ export function log(message: string, ...optionalParams: unknown[]) {
 }
 
 export function logError(message: string, ...optionalParams: unknown[]) {
-  if (__DEV__) {
-    // eslint-disable-next-line no-console
-    console.error(message, ...optionalParams)
-  }
+  // eslint-disable-next-line no-console
+  console.error(message, ...optionalParams)
 }
 
 export function logWarn(message: string, displayToast = false) {
   if (displayToast) toast({ message, type: 'warning' })
-  if (__DEV__) {
-    // eslint-disable-next-line no-console
-    console.warn(message)
-  }
+  // eslint-disable-next-line no-console
+  console.warn(message)
 }
