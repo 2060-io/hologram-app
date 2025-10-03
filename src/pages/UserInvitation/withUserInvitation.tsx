773ed45187d5b45f31eb0c336aa8ab15e9ba9c68
Daniel Fernando Rico <d.rico@mobiera.com>
2025-10-03 16:50:43 -0500

    feat: create logic to get current invitation or creates a new one if there is no stored out of band record id

--- src/pages/UserInvitation/withUserInvitation.tsx
+++ src/pages/UserInvitation/withUserInvitation.tsx
@@ -5,40 +5,71 @@ import { ConnectionInfo, UserInvitationProps, WrapperUserInvitationProps } from 
 
 import { ModalLoading } from '@2060/components/common'
 import { useMobileAgent, useUserProfile } from '@2060/hooks/agent'
-import { createInvitation } from '@2060/services/agent/oob'
+import { createInvitation, getOutOfBandRecordById } from '@2060/services/agent/oob'
+import {
+  getStorageData,
+  setStorageData,
+  USER_INVITATION_OUT_OF_BAND_RECORD_ID,
+} from '@2060/services/localStorage'
+import { logError } from '@2060/utils'
 import { toast } from '@2060/utils/toast'
 
 const withUserInvitation = (UserInvitationComponent: ElementType<UserInvitationProps>) => {
   const WrapperUserInvitation = (props: WrapperUserInvitationProps) => {
     const [creatingInvitation, setCreatingInvitation] = useState(false)
     const [connectionInfo, setConnectionInfo] = useState<ConnectionInfo>()
-
     const { userProfileData } = useUserProfile()
     const { agent } = useMobileAgent()
 
-    const handleCreateConnection = async () => {
+    useEffect(() => {
+      const setupInvitation = async () => {
+        const storedOutOfBandRecordId = (await getStorageData(
+          USER_INVITATION_OUT_OF_BAND_RECORD_ID,
+        )) as string
+        storedOutOfBandRecordId ? getCurrentInvitation(storedOutOfBandRecordId) : createNewInvitation()
+      }
+      setupInvitation()
+    }, [])
+
+    const getCurrentInvitation = async (outOfBandRecordId: string) => {
       if (!agent) return
-      setCreatingInvitation(true)
       try {
-        const invitation = await createInvitation(agent, {
-          label: userProfileData?.displayName,
-        })
+        const { outOfBandInvitation } = await getOutOfBandRecordById(agent, outOfBandRecordId)
         setConnectionInfo({
-          // TODO: Handle properly the case where no label is set
-          displayName: invitation.label ?? 'Unlabeled',
-          invitationCode: invitation.toUrl({ domain: Config.BASE_INVITATION_URL as string }),
+          displayName: outOfBandInvitation.label ?? 'Unlabeled',
+          invitationCode: outOfBandInvitation.toUrl({ domain: Config.BASE_INVITATION_URL as string }),
         })
       } catch (error) {
         props.navigation.goBack()
-        toast({ type: 'error', message: `Error creating invitation ${error}` })
+        const message = `Error getting current invitation ${error}`
+        toast({ type: 'error', message })
+        logError(message)
+      }
+    }
+
+    const createNewInvitation = async () => {
+      if (!agent) return
+      setCreatingInvitation(true)
+      try {
+        const outOfBandRecord = await createInvitation(agent, {
+          label: userProfileData?.displayName,
+        })
+        setConnectionInfo({
+          displayName: outOfBandRecord.outOfBandInvitation.label ?? 'Unlabeled',
+          invitationCode: outOfBandRecord.outOfBandInvitation.toUrl({
+            domain: Config.BASE_INVITATION_URL as string,
+          }),
+        })
+        await setStorageData(USER_INVITATION_OUT_OF_BAND_RECORD_ID, outOfBandRecord.id)
+      } catch (error) {
+        props.navigation.goBack()
+        const message = `Error creating invitation ${error}`
+        toast({ type: 'error', message })
+        logError(message)
       } finally {
         setCreatingInvitation(false)
       }
     }
-
-    useEffect(() => {
-      handleCreateConnection()
-    }, [])
 
     if (creatingInvitation || !connectionInfo) {
       return <ModalLoading visible={creatingInvitation || !connectionInfo} />
