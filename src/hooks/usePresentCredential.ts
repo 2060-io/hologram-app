5df1b55a2d703cea7c7df781e49144830e8be287
Daniel Fernando Rico <d.rico@mobiera.com>
2025-10-08 12:31:16 -0500

    refactor: create hook to present credential

---
+++ src/hooks/usePresentCredential.ts
@@ -0,0 +1,83 @@
+import { ProofState, W3cCredentialRepository } from '@credo-ts/core'
+import { ParamListBase } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack'
+import { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { AgentActionType, useChats, useMobileAgent } from '@2060/hooks/agent'
+import { AnoncredsAttribute } from '@2060/hooks/agent/actions/AgentActionExecuter'
+import { createChatEntry } from '@2060/hooks/agent/chat/services'
+import { useAgentActionQueue } from '@2060/hooks/agent/useAgentActionQueue'
+import { useLocalRealm } from '@2060/hooks/providers/RealmProvider'
+import { ChatEntryRole, ChatEntryState, ChatEntryType, VPResponseMetadata } from '@2060/model'
+import { getCredentialDetailsForDisplay, getCredentialMainInfo } from '@2060/services/agent/display'
+import { formatCredentialSubject } from '@2060/services/agent/formatCredentialSubject'
+import { toast } from '@2060/utils/toast'
+
+export const usePresentCredential = () => {
+  const { t } = useTranslation()
+  const { agent } = useMobileAgent()
+  const { realm } = useLocalRealm()
+  const { addAgentActionToQueue } = useAgentActionQueue()
+  const { findOrCreateThread } = useChats()
+
+  const present = useCallback(
+    async (
+      credentialRecordId: string,
+      connectionsId: string[],
+      navigation: StackNavigationProp<ParamListBase>,
+    ) => {
+      if (!agent || !realm) return
+      const credentialRecord = await agent.dependencyManager
+        .resolve(W3cCredentialRepository)
+        .findById(agent.context, credentialRecordId)
+      if (!credentialRecord) return
+      const mainInfo = getCredentialMainInfo(credentialRecord)
+      const credentialDefinitionId = credentialRecord.getTag('anonCredsCredentialDefinitionId') as string
+      if (!credentialDefinitionId) return
+      const credentialDetails = getCredentialDetailsForDisplay(credentialRecord)
+      const anoncredsAttributes: AnoncredsAttribute[] = []
+      const detailsSections = credentialDetails
+        ? formatCredentialSubject({ subject: credentialDetails.attributes, sanitizeKey: false })
+        : []
+      detailsSections.forEach(section => {
+        section.rows.forEach(row => {
+          anoncredsAttributes.push({ name: row.key, credentialDefinitionId })
+        })
+      })
+      connectionsId.forEach(async connectionId => {
+        const didcommConnection = await agent.connections.getById(connectionId)
+        const chatThreadId = findOrCreateThread({ connection: didcommConnection }).id
+        const metadata: VPResponseMetadata = {
+          proofState: ProofState.ProposalSent,
+          presentedCredentials: JSON.stringify([{ mainInfo }]),
+        }
+        const chatEntry = createChatEntry(realm, {
+          associatedRecordId: '',
+          chatThreadId,
+          type: ChatEntryType.VPResponse,
+          role: ChatEntryRole.Sender,
+          state: ChatEntryState.Created,
+          createdAt: new Date().getTime(),
+          metadata,
+        })
+        addAgentActionToQueue({
+          type: AgentActionType.PresentCredential,
+          chatEntryId: chatEntry.id,
+          parameters: {
+            anoncredsAttributes,
+            didcommConnectionId: connectionId,
+          },
+        })
+      })
+      toast({
+        type: 'success',
+        message: t('credential.presented'),
+      })
+      navigation.goBack()
+    },
+    [],
+  )
+
+  return { present }
+}
--- src/pages/PresentCredential/PresentCredential.tsx
+++ src/pages/PresentCredential/PresentCredential.tsx
@@ -1,4 +1,3 @@
-import { ProofState } from '@credo-ts/core'
 import { StackScreenProps } from '@react-navigation/stack'
 import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
@@ -6,78 +5,18 @@ import { useTranslation } from 'react-i18next'
 import BaseForward from '../Forward/BaseForward'
 
 import { NavigationStackParams } from '@2060/components/Navigation/NavigationProps'
-import { AgentActionType, useChats, useCredentialById, useMobileAgent } from '@2060/hooks/agent'
-import { AnoncredsAttribute } from '@2060/hooks/agent/actions/AgentActionExecuter'
-import { createChatEntry } from '@2060/hooks/agent/chat/services'
-import { useAgentActionQueue } from '@2060/hooks/agent/useAgentActionQueue'
-import { useLocalRealm } from '@2060/hooks/providers/RealmProvider'
-import { ChatEntryRole, ChatEntryState, ChatEntryType, VPResponseMetadata } from '@2060/model'
-import { getCredentialDetailsForDisplay, getCredentialMainInfo } from '@2060/services/agent/display'
-import { formatCredentialSubject } from '@2060/services/agent/formatCredentialSubject'
-import { toast } from '@2060/utils/toast'
+import { usePresentCredential } from '@2060/hooks'
 
 interface Props extends StackScreenProps<NavigationStackParams, 'PresentCredential'> {}
 
 const PresentCredential = ({ navigation, route }: Props) => {
   const { credentialRecordId } = route.params
   const { t } = useTranslation()
-  const { agent } = useMobileAgent()
-  const { realm } = useLocalRealm()
-  const credentialRecord = useCredentialById(credentialRecordId)
-  const { addAgentActionToQueue } = useAgentActionQueue()
-  const { findOrCreateThread } = useChats()
+  const { present } = usePresentCredential()
 
-  const presentCredential = useCallback(
-    (connectionsId: string[]) => {
-      if (!agent || !realm || !credentialRecord) return
-      const mainInfo = getCredentialMainInfo(credentialRecord)
-      const credentialDefinitionId = credentialRecord?.getTag('anonCredsCredentialDefinitionId') as string
-      if (!credentialDefinitionId) return
-      const credentialDetails = credentialRecord
-        ? getCredentialDetailsForDisplay(credentialRecord)
-        : undefined
-      const anoncredsAttributes: AnoncredsAttribute[] = []
-      const detailsSections = credentialDetails
-        ? formatCredentialSubject({ subject: credentialDetails.attributes, sanitizeKey: false })
-        : []
-      detailsSections.forEach(section => {
-        section.rows.forEach(row => {
-          anoncredsAttributes.push({ name: row.key, credentialDefinitionId })
-        })
-      })
-      connectionsId.forEach(async connectionId => {
-        const didcommConnection = await agent.connections.getById(connectionId)
-        const chatThreadId = findOrCreateThread({ connection: didcommConnection }).id
-        const metadata: VPResponseMetadata = {
-          proofState: ProofState.ProposalSent,
-          presentedCredentials: JSON.stringify([{ mainInfo }]),
-        }
-        const chatEntry = createChatEntry(realm, {
-          associatedRecordId: '',
-          chatThreadId,
-          type: ChatEntryType.VPResponse,
-          role: ChatEntryRole.Sender,
-          state: ChatEntryState.Created,
-          createdAt: new Date().getTime(),
-          metadata,
-        })
-        addAgentActionToQueue({
-          type: AgentActionType.PresentCredential,
-          chatEntryId: chatEntry.id,
-          parameters: {
-            anoncredsAttributes,
-            didcommConnectionId: connectionId,
-          },
-        })
-      })
-      toast({
-        type: 'success',
-        message: t('credential.presented'),
-      })
-      navigation.goBack()
-    },
-    [credentialRecord],
-  )
+  const presentCredential = useCallback((connectionsId: string[]) => {
+    present(credentialRecordId, connectionsId, navigation)
+  }, [])
 
   return (
     <BaseForward
