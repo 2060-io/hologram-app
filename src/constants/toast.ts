e5c3dbb81f2c5f87e5029980e5eb482f035d08cc
Daniel Fernando Rico León <33880481+DanielFRico@users.noreply.github.com>
2025-10-22 12:33:01 -0500

    feat: modernize toast UI (#336)
    * refactor: modernize toast (snackbar) UI
    
    * refactor: change name by Toast
    
    * fix: remove remaining unnecessary fontFamily={'EuclidCircularA-Regular'}

--- src/constants/toast.ts
+++ src/constants/toast.ts
@@ -1,16 +1,10 @@
+import { palette } from '@2060/styles'
+
 export const SHOW_TOAST_MESSAGE = 'SHOW_TOAST_MESSAGE'
 
-type Color = {
-  [type: string]: {
-    backgroundColor: string
-    color: string
-    borderColor: string
-  }
-}
-
-export const COLORS: Color = {
-  ['info']: { backgroundColor: '#cff4fc', color: '#055160', borderColor: '#b6effb' },
-  ['warning']: { backgroundColor: '#fff3cd', color: '#664d03', borderColor: '#ffecb5' },
-  ['error']: { backgroundColor: 'rgba(245, 3, 75, 0.25)', color: '#FFFFFF', borderColor: '#F5034B' },
-  ['success']: { backgroundColor: '#d1e7dd', color: '#0f5132', borderColor: '#badbcc' },
+export const COLORS = {
+  ['info']: { backgroundColor: palette.black },
+  ['warning']: { backgroundColor: palette.orange },
+  ['error']: { backgroundColor: palette.red },
+  ['success']: { backgroundColor: palette.green },
 }
