6a8185ad09bea2ec2c4deb5b76d331cb6622ffdf
Daniel Fernando Rico León <33880481+DanielFRico@users.noreply.github.com>
2026-01-07 06:31:06 -0500

    fix: remove trailing space in VCOfferChatView directory (#371)

--- src/App.tsx
+++ src/App.tsx
@@ -68,15 +68,7 @@ const App = () => {
   initializeI18n.then(() => setTranslationsLoaded(true))
 
   useEffect(() => {
-    /**
-     * FIXME: The SplashScreen.hide() was added inside a 0 timeout to
-     * works well in android. So a probably reason could be the order
-     * execution of this setTimeout in the main thread, even if timeout is 0
-     * function well be executed after main thread be free (other react native tasks finished)
-     */
-    setTimeout(() => {
-      SplashScreen.hide()
-    }, 0)
+    SplashScreen.hide()
   }, [])
 
   return (
