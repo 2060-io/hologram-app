56363e3f8dbea4d9618cfd60ed457a43315340a4
Daniel Fernando Rico <d.rico@mobiera.com>
2026-01-08 12:04:18 -0500

    fix: remove isConnectionCompleted and isConnectionBlocked from filter to render wrapped component

--- src/pages/ConnectionDetails/withRenderConnectionMainActions.tsx
+++ src/pages/ConnectionDetails/withRenderConnectionMainActions.tsx
@@ -5,7 +5,7 @@ import {
   InitialConnectionMainActionsProps,
 } from '@2060/components/common/ConnectionMainActions/Props'
 import { useConnectionById } from '@2060/hooks/agent/ConnectionProvider'
-import { isBlocked, isTerminated } from '@2060/utils/connectionUtils'
+import { isTerminated } from '@2060/utils/connectionUtils'
 
 export const withRenderConnectionMainActions =
   (Component: React.ComponentType<ConnectionMainActionsProps>) =>
@@ -13,9 +13,7 @@ export const withRenderConnectionMainActions =
     const { connectionId } = props
     const connection = useConnectionById(connectionId)
     if (!connection) return null
-    const isConnectionCompleted = connection.isReady
-    const isConnectionBlocked = isBlocked(connection)
     const isConnectionTerminated = isTerminated(connection)
-    if (!isConnectionCompleted || isConnectionBlocked || isConnectionTerminated) return null
+    if (isConnectionTerminated) return null
     return <Component {...props} connection={connection} />
   }
