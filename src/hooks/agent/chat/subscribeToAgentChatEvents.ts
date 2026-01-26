f46bcad0b8d382906c9a8f58bcc153d59af0bc25
Daniel Fernando Rico León <33880481+DanielFRico@users.noreply.github.com>
2026-01-26 16:50:36 -0500

    fix: connections are not being fully stablished when host is offline (#395)

--- src/hooks/agent/chat/subscribeToAgentChatEvents.ts
+++ src/hooks/agent/chat/subscribeToAgentChatEvents.ts
@@ -100,11 +100,11 @@ export function subscribeToAgentChatEvents(
 ) {
   getActiveChatThreadId = forceRefreshFunctionReference ? receivedGetActiveChatThreadId : () => undefined
   const mobileAgentInstance = AgentSingleton.instance
-  if (mobileAgentInstance.getIsAppSubscribedToEvents()) {
-    log('From main flow App is already subscribed to agent events')
+  if (mobileAgentInstance.getIsAppSubscribedToChatEvents()) {
+    log('From main flow App is already subscribed to agent chat events')
     return
   }
-  mobileAgentInstance.setAppIsSubscribedToEvents(true)
+  mobileAgentInstance.setAppIsSubscribedChatToEvents(true)
 
   const connectionProfileListener = async (event: ConnectionProfileUpdatedEvent) => {
     const { connection } = event.payload
@@ -314,9 +314,7 @@ export function subscribeToAgentChatEvents(
           connectionId: connection.id,
           receipts: [receipt],
         }
-        const agentActionQueueSingleton = AgentActionQueueSingleton.instance
-        agentActionQueueSingleton.configureQueue()
-        agentActionQueueSingleton.addJob({ type: AgentActionType.SendReceipts, parameters }, true)
+        AgentActionQueueSingleton.instance.addJob({ type: AgentActionType.SendReceipts, parameters })
       }
     }
   }
--- src/hooks/agent/connections/subscribeToAgentConnectionEvents.ts
+++ src/hooks/agent/connections/subscribeToAgentConnectionEvents.ts
@@ -16,20 +16,27 @@ import {
   DidExchangeState,
 } from '@credo-ts/core'
 
-import { AgentActionOptions, AgentActionType } from '../actions/AgentAction'
+import { AgentActionType } from '../actions/AgentAction'
 import {
   AcceptConnectionRequestParameters,
   AcceptConnectionResponseParameters,
   QueryServiceFeaturesParameters,
 } from '../actions/types'
 
+import { AgentActionQueueSingleton } from '@2060/services/AgentActionQueueSingleton'
+import AgentSingleton from '@2060/services/AgentSingleton'
 import { supportsUserProfile } from '@2060/utils/connectionUtils'
 import { language } from '@2060/utils/language'
+import { log } from '@2060/utils/log'
 
-export function manageAgentConnectionEvents(
-  context: AgentContext,
-  addAgentActionToQueue: (action: AgentActionOptions) => void,
-) {
+export function subscribeToAgentConnectionEvents(context: AgentContext) {
+  const mobileAgentInstance = AgentSingleton.instance
+  if (mobileAgentInstance.getIsAppSubscribedToConnectionEvents()) {
+    log('From main flow App is already subscribed to agent connection events')
+    return
+  }
+  mobileAgentInstance.setIsAppSubscribedToConnectionEvents(true)
+  const agentActionQueueSingleton = AgentActionQueueSingleton.instance
   const eventEmitter = context.dependencyManager.resolve(EventEmitter)
 
   const disclosureListener = async (event: DiscoverFeaturesDisclosureReceivedEvent) => {
@@ -44,7 +51,7 @@ export function manageAgentConnectionEvents(
 
     if (supportsUserProfile(connection)) {
       if (connection.role === DidExchangeRole.Responder) {
-        addAgentActionToQueue({
+        agentActionQueueSingleton.addJob({
           type: AgentActionType.SendUserProfile,
           parameters: {
             connectionId: connection.id,
@@ -61,7 +68,7 @@ export function manageAgentConnectionEvents(
 
   const profileRequestListener = async (event: UserProfileRequestedEvent) => {
     const userProfileApi = context.dependencyManager.resolve(UserProfileApi)
-    addAgentActionToQueue({
+    agentActionQueueSingleton.addJob({
       type: AgentActionType.SendUserProfile,
       parameters: {
         connectionId: event.payload.connection.id,
@@ -78,7 +85,7 @@ export function manageAgentConnectionEvents(
   const profileUpdatedListener = async (event: ConnectionProfileUpdatedEvent) => {
     const userProfileApi = context.dependencyManager.resolve(UserProfileApi)
     if (event.payload.sendBackYoursRequested) {
-      addAgentActionToQueue({
+      agentActionQueueSingleton.addJob({
         type: AgentActionType.SendUserProfile,
         parameters: {
           connectionId: event.payload.connection.id,
@@ -98,7 +105,7 @@ export function manageAgentConnectionEvents(
     const { connectionRecord } = event.payload
     if (connectionRecord.state === DidExchangeState.RequestReceived) {
       const parameters: AcceptConnectionRequestParameters = { connectionId: connectionRecord.id }
-      addAgentActionToQueue({
+      agentActionQueueSingleton.addJob({
         type: AgentActionType.AcceptConnectionRequest,
         parameters,
       })
@@ -107,14 +114,14 @@ export function manageAgentConnectionEvents(
       !connectionRecord.autoAcceptConnection
     ) {
       const parameters: AcceptConnectionResponseParameters = { connectionId: connectionRecord.id }
-      addAgentActionToQueue({
+      agentActionQueueSingleton.addJob({
         type: AgentActionType.AcceptConnectionResponse,
         parameters,
       })
     }
     if (connectionRecord.isReady) {
       const parameters: QueryServiceFeaturesParameters = { connectionId: connectionRecord.id }
-      addAgentActionToQueue({
+      agentActionQueueSingleton.addJob({
         type: AgentActionType.QueryServiceFeatures,
         parameters,
       })
